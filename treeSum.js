48354a0cf774b68c0337450a8fa3bcf644d6b94e
Mikalai Kalchuk <knik.f1@gmail.com>
2023-02-17 17:51:51 +0300

    Func treeSum

--- treeSum.js
+++ treeSum.js
@@ -5,3 +5,14 @@ const arr7 = [5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [0] ], 1, 8];
 const treeSum = (arr) => arr.flat(Infinity).reduce((sum, current) => sum + current, 0);
 
 console.log(treeSum(arr7));
+
+
+//2.функция treeSum
+
+const arr8 = [5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8];
+
+const treeSum = (arr) => arr.reduce((sum, current) => {
+  return sum + (Array.isArray(current) ? treeSum(current) : current)
+}, 0);
+
+console.log(treeSum(arr8));
